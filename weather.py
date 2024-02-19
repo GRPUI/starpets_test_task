3c0d52db8d0a797618abe3865f2b6f4cf74d5d1c
Yaroslav Galitsyn <galicin20045@gmail.com>
2024-02-19 10:19:04 +0700

    Add datetime saving

--- weather.py
+++ weather.py
@@ -2,18 +2,30 @@
 import requests
 import dotenv
 import os
+from datetime import datetime, timedelta
 
 # загружаем переменные окружения
 dotenv.load_dotenv()
+
+temperatures = {}
 
 
 # функция для получения погоды
 def fetch_weather(city):
     key = os.getenv('API_KEY')
+    if city in temperatures.keys():
+        delta = datetime.now() - temperatures[city]["timedelta"]
+        if delta < timedelta(minutes=10):
+            return temperatures[city]["temperature"]
+
     url = f'https://api.weatherapi.com/v1/current.json?key={key}&q={city}&aqi=no'
     response = requests.get(url)
     try:
         result = response.json()['current']['temp_c']
+        temperatures[city] = {
+            "temperature": result,
+            "timedelta": datetime.now()
+        }
     except KeyError:
         result = None
     return result
